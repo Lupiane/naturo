3c95ab6a328d1300dafe579f979365d8faa5c0bd
Pauline Gaget <lupiane@hotmail.com>
2018-08-07 17:29:39 +0200

    configuring user routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,18 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
+
+  resources :packages, only: [:show]
+
+  resources :posts, only: [:index, :show]
+
+  resources :appointments, only: [:create]
+
+  get 'contact', to: 'pages#contact'
+  get 'about', to: 'pages#about'
+  get 'testimonials', to: 'pages#testimonials'
+
+  # => NEED TO ADD ADMIN ROUTES
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
