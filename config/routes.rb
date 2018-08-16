61aaa51fb81ba7acc7f2ec493831d38a8be7b622
Pauline Gaget <lupiane@hotmail.com>
2018-08-16 15:26:01 +0200

    configuring i18n for the app

--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,22 @@
 Rails.application.routes.draw do
   mount Lines::Engine => "/blog"
   devise_for :users
-  root to: 'pages#home'
 
-  get 'contact', to: 'pages#contact'
-  get 'about', to: 'pages#about'
-  get 'testimonials', to: 'pages#testimonials'
+  scope '(:locale)', locale: /en/ do
+    root to: 'pages#home'
 
-  resources :packages
+    get 'contact', to: 'pages#contact'
+    get 'about', to: 'pages#about'
+    get 'testimonials', to: 'pages#testimonials'
 
-  resources :appointments do
-    collection do
-      get 'first', to: 'appointments#first'
+    resources :packages
+
+    resources :appointments do
+      collection do
+        get 'first', to: 'appointments#first'
+      end
     end
+
   end
-
-
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
