0756f1354299a452d143fa63c8bd5dadd1c80c15
Pauline Gaget <lupiane@hotmail.com>
2019-03-16 12:10:34 +0100

    only display active packages

--- app/controllers/packages_controller.rb
+++ app/controllers/packages_controller.rb
@@ -2,7 +2,7 @@ class PackagesController < ApplicationController
 skip_before_action :authenticate_user!, only: [:show, :index]
 
   def index
-    @packages = Package.all
+    @packages = Package.where(active: true)
     #remember to add filter to show only "active" packages
   end
 
